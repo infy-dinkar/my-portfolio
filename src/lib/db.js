f299cfd400672132534847f873bc9a30a59367d4
Dinkar Thakur <322dinkar@gmail.com>
2025-12-24 00:49:40 +0530

    Refactor: Use environment variable for MongoDB URI

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = "mongodb+srv://322dinkar_db_user:eyvZw86kDTha6tuY@contact.p1mb72p.mongodb.net/?appName=contact";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
     throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
